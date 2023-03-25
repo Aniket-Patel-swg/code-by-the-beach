5b31f0eb14320c14fed59fef1e7839e6a779313a
Saurav-Navdhare <navdharesaurav2003@gmail.com>
2023-03-25 16:37:50 +0530

    fixed hardhat config for localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,49 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
-// require('dotenv').config()
-
-// module.exports = {
-//   defaultNetwork: 'localhost',
-//   networks: {
-//     localhost: {
-//       url: 'http://127.0.0.1:8545',
-//     },
-//   },
-//   solidity: {
-//     version: '0.8.11',
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//     },
-//   },
-//   paths: {
-//     sources: './src/contracts',
-//     artifacts: './src/abis',
-//   },
-//   mocha: {
-//     timeout: 40000,
-//   },
-// }
-
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+require('@nomiclabs/hardhat-waffle')
+require('dotenv').config()
 
 module.exports = {
-  defaultNetwork: "matic",
+  defaultNetwork: 'localhost',
   networks: {
-    hardhat: {},
-    polygon_mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY],
+    localhost: {
+      url: 'http://127.0.0.1:7545',
     },
   },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
-  },
   solidity: {
-    version: "0.8.9",
+    version: '0.8.11',
     settings: {
       optimizer: {
         enabled: true,
@@ -52,10 +18,10 @@ module.exports = {
     },
   },
   paths: {
-    sources: "./src/contracts",
-    artifacts: "./src/abis",
+    sources: './src/contracts',
+    artifacts: './src/abis',
   },
   mocha: {
     timeout: 40000,
   },
-};
+}
